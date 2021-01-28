b78fafba48ca6aeb8d308e027dcf366b7721cf14
Antony Jones <antony@beyonk.com>
2021-01-28 15:11:26 +0000

    fix rounding

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict'
 
-require('dotenv')
+require('dotenv').config()
 const fetch = require('node-fetch')
 const { transform } = require('reorient')
 const Max7219 = require('max7219-display')
 
+const local = process.env.LOCAL_DEV
 const controllerCount = 4
-const display = new Max7219({ device: '/dev/spidev0.0', controllerCount })
+const display = !local &&new Max7219({ device: '/dev/spidev0.0', controllerCount })
 
 const list = [
   { symbol: 'GME' },
@@ -17,16 +18,19 @@ const list = [
 const globalPath = 'Global Quote'
 const transformer = {
   symbol: j => j[globalPath]['01. symbol'],
-  price: j => j[globalPath]['05. price'],
-  change: j => j[globalPath]['09. change'],
-  percent: j => j[globalPath]['10. change percent'],
+  price: j => round(j[globalPath]['05. price']),
+  change: j => round(j[globalPath]['09. change']),
+  percent: j => round(j[globalPath]['10. change percent'].slice(0, -1)) + '%',
   growth: j => j[globalPath]['09. change'] >= 0
+}
+
+function round (num) {
+  return Math.round(parseFloat(num) * 100) / 100
 }
 
 async function fetchStonk (symbol) {
   const res = await fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${process.env.API_KEY}`)
   const json = await res.json()
-  console.log(json)
   return transform(json, transformer)
 }
 
@@ -35,21 +39,25 @@ async function getStonks (list) {
 }
 
 async function parseStonks (stonks) {
-  return stonks.map(({ symbol, price, change, percent }) => `${symbol} $${price} ${change} (${percent})`).join(' ')
+  return stonks.map(({ symbol, price, change, percent, growth }) => {
+    const direction = growth ? '+' : ''
+    return `${symbol} $${price} ${direction}${change} (${direction}${percent})`
+  }).join(' ')
 }
 
 async function showStonks (line) {
   for (let i = 0; i < controllerCount; i++) {
     await display.reset(i)
   }
-  await display.scroll(line, { scrollIn: true, loop: true, speed: 100 })
+  await display.scroll(line, { scrollIn: true, loop: true, speed: 200 })
   await display.resetAll()
 }
 
 async function tick () {
   const stonks = await getStonks(list)
   const line = await parseStonks(stonks)
-  await showStonks(line)
+  local && console.log(line)
+  !local && await showStonks(line)
 }
 
 tick()
